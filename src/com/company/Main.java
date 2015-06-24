f684a7c3f11910899091b3dbccb78f83082233db
Your Name <sazanovich.aleks@gmail.com>
2015-06-24 11:34:06 +0300

    Удаление файлов netbeans

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.lang.System;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+//        System.setOut("Hello World");
+//        System.out.println("Hello World");
+
     }
 }
