d184d90f7d514a85604461be1aef159724dee5b9
Your Name <sazanovich.aleks@gmail.com>
2015-07-13 18:05:36 +0300

    addded class BinarySearch

--- src/alex/learn/BinarySearch.java
+++ src/alex/learn/BinarySearch.java
@@ -1,38 +1,17 @@
 package alex.learn;
 
-import java.util.Arrays;
-
-import static java.lang.Math.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by alex on 24.06.15.
  */
 public class BinarySearch {
 
-    public int []arrKeys;
-    public int size;
+  public static int binarySearch(int key, int[] arr, int min, int max){
 
-    public  BinarySearch(int size) {
-        this.size = size;
-        arrKeys = new int[size];
-        this.generateValues();
-        Arrays.sort(arrKeys);
+      return -1;
+  }
 
-    }
-
-    public void generateValues(){
-        for(int i=0;i<this.size;i++){
-            arrKeys[i] = (int) (random() * 100);
-//            Arrays.
-        }
-    }
-
-
-    @Override
-    public String toString() {
-        return "BinarySearch{" +
-                "arrKeys=" + Arrays.toString(arrKeys) +
-                ", size=" + size +
-                '}';
-    }
 }
--- src/alex/learn/Main.java
+++ src/alex/learn/Main.java
@@ -1,12 +1,29 @@
 package alex.learn;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by alex on 24.06.15.
  */
 public class Main {
 
+    public static void printArray(int [] array, int size) {
+        for (int i=0;i<size;i++) {
+            System.out.println(array[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        BinarySearch BinarySearch = new BinarySearch(100);
-        System.out.println(BinarySearch.toString());
+        int [] arr = new int[100];
+
+        for(int i = 0; i< 100; i++){
+            arr[i] = (int) (Math.random() * 100);
+        }
+        Arrays.sort(arr);
+        System.out.println(arr);
+        Main.printArray(arr,100);
+
     }
 }
